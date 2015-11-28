87c5d735a803cb82c92ac9a5943cc6f39333ed53
Scott Christensen <scottmc526@gmail.com>
2015-11-28 13:18:20 -0700

    01 exercies complete

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,41 @@
 module.exports = {
   sameLength: function(string1, string2) {
-    // your code here:
-  }
+    if (string1.length === string2.length) {
+      return true;
+      } else {
+        return false;
+      }
+    },
+
+    firstGreater: function(string1, string2) {
+      if (string1 > string2) {
+        return true;
+      } if (string1 <= string2) {
+        return false;
+      }
+    },
+
+    firstGreaterOrEqualTo: function(string1, string2) {
+      if (string1 >= string2) {
+        return true;
+      } if (string1 < string2) {
+        return false;
+      }
+    },
+
+    firstLengthGreater: function(string1, string2) {
+      if (string1.length > string2.length) {
+        return true;
+      } if (string1.length >= string2.length) {
+        return false;
+      }
+    },
+
+    secondGreater: function (string1, string2) {
+      if (string2 > string1) {
+        return true;
+      } if (string1 >= string2) {
+        return false;
+      }
+    },
 }
