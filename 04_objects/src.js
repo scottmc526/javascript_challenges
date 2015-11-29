315888da7dc4d687a636c4bb13818272c6395a80
Scott Christensen <scottmc526@gmail.com>
2015-11-29 11:16:37 -0700

    objects through keys

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,31 @@
 module.exports = {
-  person: function() {
-    // your code here
-  }
+  person: function(firstName, lastName) {
+    var person = new Object ();
+    person.firstName = "John";
+    person.lastName = "Mayer";
+    return person;
+  },
+
+  sweetnessProperty: function(obj) {
+    return obj.sweetness;
+    if (obj.sweetness == undefined) {
+      return undefined;
+    }
+  },
+  savorynessProperty: function(obj) {
+    return obj['savory-ness'];
+    if (obj['savory-ness'] === undefined) {
+      return undefined;
+    }
+  },
+  keys: function (obj) {
+    var keys = [];
+    for (var key in obj) {
+      keys.push(key);
+    }
+    return keys;
+    if (obj === {}) {
+      return [];
+    }
+  },
 }
