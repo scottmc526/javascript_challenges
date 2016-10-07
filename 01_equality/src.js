ee3c1cd81cf1232dd697704d91f539a3afc3bb40
Scott Christensen <scottmc526@gmail.com>
2016-10-06 18:40:23 -0600

    sameLength refactor

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,11 +1,8 @@
 module.exports = {
+
   sameLength: function(string1, string2) {
-    if (string1.length === string2.length) {
-      return true;
-      } else {
-        return false;
-      }
-    },
+    return string1.length === string2.length;
+  },
 
     firstGreater: function(string1, string2) {
       if (string1 > string2) {
